00d267be2aa5814415120fa7fe1e715e514cb42f
rizingson <damonjbonner@gmail.com>
2019-06-18 03:12:12 -0700

    Add files via upload

---
+++ views/About.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container fluid class="about">
+    <v-layout column>
+      <v-flex xs6 md6 mt-10 align-self-center>
+        <v-card xs4 color="#242526" dark class="about-card border">
+          <v-img :src="require('../assets/rolls.jpg')" contain />"
+          <v-card-text class="headline text-xs-center text-uppercase">
+            The first and most established of Merced's Sushi restaurants.
+            <hr />
+          </v-card-text>
+          <v-card-text class="subheading mt-2 text-xs-center">
+            King's Asian Cuisine provides its customers with friendly service,
+            great food, and a positive atmosphere. We welcome families and
+            individuals of all ages as we have something for anybody in the
+            group. We serve both Chinese and Japanese Cuisine ranging from fried
+            rice to sushi rolls and sashimi.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn flat dark color="white" to="/">Back To Home</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "About",
+  components: {}
+};
+</script>
+
+<style>
+.about {
+  background: url("../assets/swirl.jpg");
+  height: 100%;
+  width: 100%;
+}
+.about-card {
+  width: 500px;
+}
+</style>
---
+++ views/location.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-container fluid fill-height class="about">
+        <v-layout column>
+            <v-flex xs6 md6 align-self-center>
+                <v-card xs4 color="#242526" dark class="about-card border">
+                    <v-img
+                        :src="require('../assets/storeFront.jpg')"
+                        contain
+                    />"
+                    <v-card-text class="headline text-xs-center text-uppercase">
+                        Location and Hours
+                        <hr />
+                    </v-card-text>
+                    <v-card-text class="subheading mt-2 text-xs-center">
+                        360 W Main St. <br />
+                        Merced, CA 95340<br />
+                        <a class="link" href="tel:209383983">(209) 383-9838</a>
+                        <br />
+                        <br />
+                        <strong>Business Hours:</strong><br />
+                        Tuesday-Thursday: 11am to 9pm<br />
+                        Friday: 11am to 10pm<br />
+                        Saturday: 12pm to 10pm<br />
+                        Sunday: 12pm to 9pm
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn flat dark color="white" to="/"
+                            >Back To Home</v-btn
+                        >
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+
+
+export default {
+    name: 'location',
+    components: {}
+};
+</script>
+
+<style></style>
